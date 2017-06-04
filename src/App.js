b01acf38678aac2d3eee4104147c21103423465f
aleexga <aleexga@gmail.com>
2017-06-04 18:56:01 -0500

    Setup
    Created dependencies
    Imported components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 import React from "react";
 import TopNav from "./components/TopNav";
 import SideNav from "./components/SideNav";
+import Charts from "./components/Charts";
+import Tables from "./components/Tables";
+import Settings from "./components/Settings";
+import Wall from "./components/Wall";
+import Profiles from "./components/Profiles";
+import Marquee from "./components/Marquee";
+import Dashboard from "./components/Dashboard";
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div> 
-      <div id="wrapper">
-        <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
-          <TopNav />
-          <SideNav />
-        </nav>
-        <div style={{backgroundColor: "white"}}>
-          {/* PUT YOUR ROUTES HERE */}
+    <BrowserRouter>
+      <div>
+        <div id="wrapper">
+          <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <TopNav />
+            <SideNav />
+          </nav>
+          <div style={{backgroundColor: "white"}}>
+            <Route path="/charts" component={Charts} />
+            <Route path="/tables" components={Tables} />
+            <Route path="/settings" components={Settings} />
+            <Route path="/wall" components={Wall} />
+            <Route path="/profiles" components={Profiles} />
+            <Route path="/marquee/:text" components={Marquee} />
+            <Route path="/profile/:id" components={Profiles} />
+            <Route path="/" components={Dashboard} />
+          </div>
         </div>
       </div>
-    </div>
+    </BrowserRouter>
 
   );
 }
